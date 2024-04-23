09534c1d9a1c5e883ae93cc33b59223ea1559550
TarshSeriy <tarsergey5@gmail.com>
2024-04-24 01:55:56 +0500

    start

---
+++ src/org/example/utils/interfaces/MyQueueInterface.java
@@ -0,0 +1,12 @@
+package org.example.utils.interfaces;
+
+import java.util.NoSuchElementException;
+
+public interface MyQueueInterface<T> {
+    void enqueue(T item);
+    T dequeue();
+    T peek();
+    boolean isEmpty();
+    int size();
+    void clear();
+}
